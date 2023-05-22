5f199a1961c54d396685d6330d2d5bfce62b6837
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2023-05-22 21:34:37 +0300

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -42,31 +42,6 @@ public class Block {
         final int WINDOW_WIDTH = 1200;
         final int WINDOW_HEIGHT = 600;
 
-        // Parse the input to get block details
-        /*String input = "block type = Sum, name = Add, ID = 3, position = [1040, 209, 1070, 241], zOrder = 3\n" +
-                "attributes: {Ports=[3, 1], IconShape=rectangular, Inputs=+++}";*/
-        /*String input = "block type = Sum, name = Add, ID = 3, position = [1040, 209, 1070, 241], zOrder = 3\n" +
-                "attributes: {Ports=[3, 1], IconShape=rectangular, Inputs=+++}";
-        int i=0;
-        String type = input.substring(input.indexOf('=',i)+2,input.indexOf(',',i));
-        i=input.indexOf(',',i)+1;
-        String name = input.substring(input.indexOf('=',i)+2,input.indexOf(',',i));
-        i=input.indexOf(',',i)+1;
-        int ID = Integer.parseInt(input.substring(input.indexOf('=',i)+2,input.indexOf(',',i)));
-        i=input.indexOf(',',i)+1;
-        String position = input.substring(input.indexOf('[',i)+1,input.indexOf(']',i));
-        i=input.indexOf(']',i)+1;
-        int zOrder = Integer.parseInt(input.substring(input.indexOf('=',i)+2,input.indexOf("attributes")-1));
-
-        i=0;
-        int x1 = Integer.parseInt(position.substring(0,position.indexOf(',',i)));
-        i=position.indexOf(',',i)+1;
-        int y1 = Integer.parseInt(position.substring(i+1,position.indexOf(',',i)));
-        i=position.indexOf(',',i)+1;
-        int x2 = Integer.parseInt(position.substring(i+1,position.indexOf(',',i)));
-        i=position.indexOf(',',i)+1;
-        int y2 = Integer.parseInt(position.substring(i+1,position.length()));*/
-
         //Create a rectangle
         double rectangleWidth = position[2]-position[0];
         double rectangleHeight = position[3]-position[1];
@@ -89,34 +64,9 @@ public class Block {
         }catch(Exception e){
             blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Error.png");
         }
-        /*if(this.blockType.equals("Sum"))
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Sum.png");
-        else if(this.blockType.equals("Constant"))
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Constant.png");
-        else if(this.blockType.equals("Saturate"))
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Saturate.png");
-        else if(this.blockType.equals("Scope"))
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Scope.png");
-        else if(this.blockType.equals("UnitDelay"))
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\UnitDelay.png");
-        else
-            blockImage = new Image("C:\\Users\\aliom\\OneDrive\\Pictures\\Saved Pictures\\Error.png");*/
-
-        // Calculate the image size to be slightly smaller than the rectangle
-
-        // Calculate the image size to be slightly smaller than the rectangle
-        /*double imageWidth = rectangleWidth - (5 * borderWidth);
-        double imageHeight = rectangleHeight - (5 * borderWidth);*/
 
         ImagePattern imagePattern = new ImagePattern(blockImage);
         rectangle.setFill(imagePattern);
-
-        // Create an ImageView for the block background
-        /*ImageView blockImageView = new ImageView(blockImage);
-        blockImageView.setFitWidth(x2 - x1);
-        blockImageView.setFitHeight(y2 - y1);
-        blockImageView.setLayoutX(x1/2);
-        blockImageView.setLayoutY(y1);*/
 
         // Create a label for the block name
         Label nameLabel = new Label(this.name);
@@ -141,10 +91,5 @@ public class Block {
             rectangle.setLayoutY(newY);
             nameLabel.setLayoutY(newY + (position[3]-position[1]) + 5);
         });
-
-        /*Scene scene = new Scene(pane);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Block Viewer");
-        primaryStage.show();*/
     }
 }
